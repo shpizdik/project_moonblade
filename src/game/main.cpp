bcbf75e3f613f02393b5ce25e24a70bff8527d5e
shpizdik <ladipostumus@gmail.com>
2026-09-13 18:18:17 +0300

    wow u made a window u are so unique

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -4,7 +4,35 @@
 
 using namespace std;
 
-int main(){
-	cout << "henlo wrld" << endl;
+int main(void){
+	// Constants
+	//----------------------------------------------------------------------
+	const int PRM_SCREEN_WIDTH = 1280;
+	const int PRM_SCREEN_HEIGHT = 720;
+	const int PRM_DEFAULT_FPS = 60; //Constants placed here for easy access
+
+	// Window Initialization
+	//------------------------------------------------------------
+	InitWindow(PRM_SCREEN_WIDTH, PRM_SCREEN_HEIGHT, "gavno suka");
+	SetTargetFPS(PRM_DEFAULT_FPS);
+
+	// Game Loop
+	//-----------------------------------------------------------
+	while(!WindowShouldClose()){
+		//variable updating
+
+		// Draw
+		//-------------------------------------------------------
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText("idi nahui gandon", 190, 200, 0, LIGHTGRAY);
+		EndDrawing();
+
+	}
+
+	// De-Initialization
+	//------------------
+	CloseWindow();
+
 	return 0;
 }
